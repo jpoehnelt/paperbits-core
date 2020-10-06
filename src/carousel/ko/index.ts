f1be347c3ce2adfe7b715383ceae790e07c2b2d3
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-10-06 10:03:06 -0700

    tmp

--- src/carousel/ko/index.ts
+++ src/carousel/ko/index.ts
@@ -1,6 +1,5 @@
 export * from "./carousel.module";
 export * from "./carouselEditor.module";
 export * from "./carouselEditor";
-export * from "./carouselLayoutSelector";
 export * from "./carouselViewModel";
 export * from "./carouselViewModelBinder";
