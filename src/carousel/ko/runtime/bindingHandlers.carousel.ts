8507c6c8da6a53b3aae5447ba38e335375ba694f
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2020-10-09 09:59:47 -0700

    tmp

--- src/carousel/ko/runtime/bindingHandlers.carousel.ts
+++ src/carousel/ko/runtime/bindingHandlers.carousel.ts
@@ -1,30 +1,14 @@
 import * as ko from "knockout";
 
-
 ko.bindingHandlers["carousel"] = {
-    init: (element: HTMLElement, valueAccessor: () => (data: any) => void) => {
-        console.log("QQQQ");
+    init(element: HTMLElement, valueAccessor: () => any): void {
+        const carouselItems = element.querySelectorAll(".carousel-item");
+        console.log(carouselItems);
+
+        if (carouselItems.length === 0) {
+            return;
+        }
+
+        carouselItems[0].classList.add("active");
     }
-}
-
-console.log("REG");
-
-// import * as ko from "knockout";
-// debugger;
-
-// ko.bindingHandlers["carousel1"] = {
-//     init(element: HTMLElement, valueAccessor: () => any): void {
-
-//         debugger;
-//         setTimeout(() => {
-//             const carouselItems = element.querySelectorAll(".carousel-item");
-//             console.log(carouselItems);
-
-//             if (carouselItems.length === 0) {
-//                 return;
-//             }
-
-//             carouselItems[0].classList.add("active");
-//         }, 1000);
-//     }
-// };
+};
