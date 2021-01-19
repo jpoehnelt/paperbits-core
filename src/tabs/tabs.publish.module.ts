333994d833b9549da385f418476818a72759fa1a
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2021-01-18 17:22:02 -0800

    tmp

--- src/tabs/tabs.publish.module.ts
+++ src/tabs/tabs.publish.module.ts
@@ -2,11 +2,13 @@ import { IInjectorModule, IInjector } from "@paperbits/common/injection";
 import { TabsViewModel } from "./ko/tabs";
 import { TabsModelBinder } from "./tabsModelBinder";
 import { TabsViewModelBinder } from "./ko/tabsViewModelBinder";
+import { TabPanelStyleHandler } from "./tabsStyleHandler";
 
 export class TabsPublishModule implements IInjectorModule {
     public register(injector: IInjector): void {
         injector.bind("tabs", TabsViewModel);
         injector.bindToCollection("modelBinders", TabsModelBinder, "tabsModelBinder");
         injector.bindToCollection("viewModelBinders", TabsViewModelBinder);
+        injector.bindToCollection("styleHandlers", TabPanelStyleHandler);
     }
 }
